0a0ce398f58b6ee426c3cf20d024b75c332813e6
grant <grantmossdev@gmail.com>
2021-07-05 11:00:14 -0600

    set up webpack/babel workflow

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const compression = require("compression");
+const path = require("path");
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+function shouldCompress(req, res) {
+  if (req.headers["x-no-compression"]) {
+    // don't compress responses with this request header
+    return false;
+  }
+  // fallback to standard filter function
+  return compression.filter(req, res);
+}
+app.use(compression({ filter: shouldCompress }));
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(path.resolve(__dirname, "dist")));
+
+// send the user to index html page inspite of the url
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist/index.html"));
+});
+
+app.listen(port);
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // mode: "development",
+  mode: "production",
+  entry: { index: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+
+  devServer: {
+    index: "index.html",
+    compress: true,
+    contentBase: "./dist",
+    publicPath: "/",
+    hot: true,
+  },
+
+  devtool: false,
+
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "main.css"),
+          to: path.resolve(__dirname, "dist", "main.css"),
+        },
+      ],
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: `index.html`,
+      chunks: [`index`],
+      chunksSortMode: "auto",
+      template: "src/index.html",
+      minify: {
+        collapseWhitespace: true,
+        keepClosingSlash: false,
+        removeComments: false,
+        removeRedundantAttributes: false,
+        removeScriptTypeAttributes: false,
+        removeStyleLinkTypeAttributes: false,
+        useShortDoctype: false,
+      },
+    }),
+  ],
+
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  optimization: {
+    minimize: true,
+    usedExports: true,
+  },
+};
