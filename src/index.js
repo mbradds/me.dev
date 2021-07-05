0f0df4f3bc5beb7c87d216f49867d96cba772d3c
grant <grantmossdev@gmail.com>
2021-07-05 16:11:57 -0600

    basic nav functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello personal website!");
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// function controlTabs(evt) {
+//   document
+//     .getElementById("navbarNavAltMarkup")
+//     .addEventListener("click", function (event) {
+//       console.log(event);
+//       console.log(event.target.href);
+//       var i;
+//       var tablinks = document.getElementsByClassName("nav-link");
+//       for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//       }
+//       event.target.className += " active"
+//     });
+//   return undefined;
+// }
+
+// controlTabs();
